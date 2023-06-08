ce823e33ad093066ca59acb2488d49b897781fdc
MTolmachev <mikhaeltolmachev@gmail.com>
2023-06-08 14:21:22 +0300

    refresh

--- Assets/Scripts/Spring.cs
+++ Assets/Scripts/Spring.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Spring : MonoBehaviour {
     private Rigidbody SpringRigidbody;
-    [SerializeField] float force;
+    [SerializeField] float _force = 25;
 
     void Start() {
         SpringRigidbody = GetComponent<Rigidbody>();
@@ -11,6 +11,6 @@ public class Spring : MonoBehaviour {
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
-            SpringRigidbody.AddForce(0, force * 1000, 0);
+            SpringRigidbody.AddForce(0, _force * 1000, 0);
     }
 }
